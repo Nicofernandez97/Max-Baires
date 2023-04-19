26920444052b5fed1d5d9de8a7157d607e9cbc96
NicolasFernandez <nicofernandez97@hotmail.com>
2023-04-19 19:10:27 -0300

    Modal Full

--- js/carrito.js
+++ js/carrito.js
@@ -16,20 +16,90 @@ const validacionRepetido = (idPaquete) => {
         agregarAlContenedorModal(buscadorPaquetes)
     }
     else {
-        const paqueteRepetido = paquetes.find(paquete => paquete.id == idPaquete)
+        const paqueteRepetido = paquetes.find(paquete => paquete.id == idPaquete)       //Se busca cual es el paquete repetido. Al tenerlo, se le cambia la cantidad de su propiedad personas en 1 y se actualizan los subtotales.
+        const cantidadRepetido = document.getElementById(`personas${paqueteRepetido.id}`)
+        paqueteRepetido.personas++
+        cantidadRepetido.innerHTML = ` <p>Estas reservando para: ${paqueteRepetido.personas} personas </p>
+        <p><strong> Subtotal ${paqueteRepetido.nombre}</strong>: ${paqueteRepetido.personas * paqueteRepetido.precio} Pesos argentinos </p>
+
+        `
+        carritoLive(carrito)
     }
 }
-const agregarAlContenedorModal = (paquetes) => {
+const agregarAlContenedorModal = (paquetes) => {                    //Creación y adición elementos Div con descripciones de paquetes post validación repetido. 
     const contenedorModal = document.getElementById("contenido-modal")
     const divModal = document.createElement("div")
     divModal.classList.add("col-xl-6")
     divModal.innerHTML = ` 
-    <button type="button" class="btn-close eliminar-modal " data-bs-dismiss="moda" aria-label="Close"></button>
+    <button type="button" class="eliminar-modal btn-close" value = "${paquetes.id}"></button> 
     <p><strong>${paquetes.nombre}</strong></p>
     <p><strong> Descripción</strong>: <br>${paquetes.detalles} </p>
     <p><strong> Precio:</strong> ${paquetes.precio} pesos por persona </p>
-    <p><strong> Estas reservando para: </strong>${paquetes.personas} personas</p>
-
+    <p id=personas${paquetes.id}><strong> Estas reservando para: </strong>${paquetes.personas} personas</p>    
+    
 `
     contenedorModal.appendChild(divModal)
+    carritoLive(carrito)
+}
+const carritoLive = (carrito) => {
+    const contadorMontoCarrito = carrito.reduce((acc, paq) => acc + paq.personas, 0)
+    const sumaMontoTotal = carrito.reduce((acc, paq) => acc + (paq.precio * paq.personas), 0)
+    pintarTotalesCarrito(contadorMontoCarrito, sumaMontoTotal)
+    guardarEnLocalStorage(carrito)
+    console.log(localStorage)
+}
+const pintarTotalesCarrito = (contadorMontoCarrito, sumaMontoTotal) => {
+    const contadorVacio = document.getElementById("carrito-mostrar-cantidad")
+    const sumaPrecioVacio = document.getElementById("sumaPrecioAModificar")
+    if (contadorMontoCarrito == 1) {
+        contadorVacio.innerText = ` ${carrito.length} Paquete en carrito`
+    }
+    else if (contadorMontoCarrito == 0) {
+        contadorVacio.innerText = ` Carrito Vacío`
+    }
+    else {
+        contadorVacio.innerText = ` ${carrito.length} Paquetes en carrito`
+    }
+    sumaPrecioVacio.innerText = `$ ${sumaMontoTotal} Pesos argentinos`
+}
+
+const encontrarBotonEliminado = document.querySelector(".modal-caja")
+encontrarBotonEliminado.addEventListener("click", (e) => {
+    e.stopPropagation()
+    if (e.target.classList.contains("eliminar-modal")) {
+        eliminarPaqueteCarrito(e.target.value)
+    }
+})
+const eliminarPaqueteCarrito = (paqueteID) => { //Esta función busca al paquete a eliminar por su ID y lo splicea, ejecutando el reseteo tanto de la escritura de carrito en el DOM (vía reseteoDomPostQuite) y la actualización de valores subtotales y de cantidad de productos en el carrito (vía carritoLive)   
+    const paquetePorId = carrito.findIndex(paquete => paquete.id == paqueteID) // ATENCION: NO USAR === 
+    carrito.splice(paquetePorId, 1)
+    reseteoDomPostQuite(carrito)
+    carritoLive(carrito)
+}
+
+const reseteoDomPostQuite = (carrito) => {  //Esta función limpa los valores que se crearon antes vía dom (vía .innerHTML = "") y los recrea. Su función es actualizar los valores de Carrito en el DOM.
+    const contenedorModal = document.getElementById("contenido-modal")
+    contenedorModal.innerHTML = ""   //Reseteo del DOM del modal
+    carrito.forEach(paquetes => {
+        const divModal = document.createElement("div")
+        divModal.classList.add("col-xl-6")
+        divModal.innerHTML = ` 
+    <button type="button" class="eliminar-modal btn-close"  value="${paquetes.id}"></button> 
+    <p><strong>${paquetes.nombre}</strong></p>
+    <p><strong> Descripción</strong>: <br>${paquetes.detalles} </p>
+    <p><strong> Precio:</strong> ${paquetes.precio} pesos por persona </p>
+    <p id=personas${paquetes.id}><strong> Estas reservando para: </strong>${paquetes.personas} personas</p>    
+`
+        contenedorModal.appendChild(divModal)
+    });
+}
+
+const guardarEnLocalStorage = (carrito) => {   //Utilizada en carritoLive, cada vez que se realiza un cambio se actualiza el localStorage para incluír los cambios.
+    const carritoString = JSON.stringify(carrito)
+    localStorage.setItem("carrito", carritoString)
+}
+const obtenerDelLocalStorage = () => {
+    const recuperarCarrito = localStorage.getItem("carrito")
+    recuperarCarrito = JSON.parse(recuperarCarrito)
+    return recuperarCarrito
 }
--- js/paquetesDom.js
+++ js/paquetesDom.js
@@ -1,11 +1,11 @@
-const llamadoProductos = () => {
+const llamadoProductos = () => { //Agrega productos del array paquetes (Creado en paquetes.js) al HTML de sectorcompras.
 
-    const contenedorDom = document.getElementById("contenedor-dom")
+  const contenedorDom = document.getElementById("contenedor-dom")
 
-    paquetes.forEach(paquete => {
-        const div = document.createElement("div")
-        div.className = "col-sm-12 col-xl-6"
-        div.innerHTML = `
+  paquetes.forEach(paquete => {
+    const div = document.createElement("div")
+    div.className = "col-sm-12 col-xl-6"
+    div.innerHTML = `
         <div class="card text-center border-warning card">
               <img src="${paquete.imagen}" class="card-img-top">
               <div class="card-body">
@@ -13,10 +13,11 @@ const llamadoProductos = () => {
                 <p class="card-text">${paquete.detalles} </p>
                 <p class="card-text"> Precio: $ ${paquete.precio} Por persona </p>
                 <a id=${paquete.id} class="btn btn-primary transformacion modalAdd">Reserva!</a>
-              </div>
-              </div>
-        `
-        contenedorDom.appendChild(div)
-    })
+              </div >
+              </div >
+  `
+    contenedorDom.appendChild(div)
+  })
 }
 llamadoProductos()
+
